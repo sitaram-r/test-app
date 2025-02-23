529aa0c5bd142972eef3ba26bc1f76d8eeca9f73
Sitaram Remalkar <148528188+sitaram-r@users.noreply.github.com>
2025-02-23 11:25:26 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure it works on any subdomain
+  server: {
+    port: 4173, // Dev server port (local use only)
+  },
 });
